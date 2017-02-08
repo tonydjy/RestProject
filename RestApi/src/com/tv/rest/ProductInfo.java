51edf211d683d907c492437518f8c6fc4c44b251
tonydjy <tonydjy@163.com>
2017-02-08 20:40:02 +0800

    add some sample java file

--- RestApi/src/com/tv/rest/ProductInfo.java
+++ RestApi/src/com/tv/rest/ProductInfo.java
@@ -2,11 +2,30 @@ package com.tv.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;  
+import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import javax.ws.rs.Consumes;  
 
 @Path("/hello")
 public class ProductInfo {
+	
 	@GET
-	public String hello(){
-		return "Hello TV!!";
+    @Consumes(MediaType.APPLICATION_JSON)  
+    @Produces(MediaType.APPLICATION_JSON)  
+	public JSONObject hello(){
+		JSONObject resp = new JSONObject();
+		User user = new User();
+		user.setAge(10);
+		try {
+			resp.put("name", "tv from server");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		return resp;
 	}
 }
---
+++ RestApi/src/com/tv/rest/User.java
@@ -0,0 +1,15 @@
+package com.tv.rest;
+
+public class User {
+	private int age;
+	private String name;
+	
+	public void setAge(int age){
+		this.age = age;
+	}
+	
+	public int getAge(){
+		return age;
+		
+	}
+}
