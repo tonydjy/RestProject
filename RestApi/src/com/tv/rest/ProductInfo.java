eed667dd123a0c0765d3f9c3f83049bfacd14e1b
tonydjy <tonydjy@163.com>
2017-02-14 22:14:23 +0800

    change to fastjson and add delete product method

--- RestApi/src/com/tv/rest/ProductInfo.java
+++ RestApi/src/com/tv/rest/ProductInfo.java
@@ -3,12 +3,14 @@ package com.tv.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;  
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -18,7 +20,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import javax.ws.rs.Consumes;  
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;  
 
 @Path("/product")
 public class ProductInfo {
@@ -93,7 +96,7 @@ public class ProductInfo {
 			while ((temp_str = br.readLine()) != null){
 				full_content = full_content + temp_str;
 			}
-			JSONObject dataJson = new JSONObject(full_content);
+			JSONObject dataJson =JSON.parseObject(full_content);
 			JSONArray productList = dataJson.getJSONArray("ProductList");
 			resp.put("ProductList", productList);
 			br.close();
@@ -138,9 +141,60 @@ public class ProductInfo {
 				full_content = full_content + temp_str;
 			}
 			br.close();
-			JSONObject dataJson = new JSONObject(full_content);
+			JSONObject dataJson = JSON.parseObject(full_content);
 			JSONArray productList = dataJson.getJSONArray("ProductList");
-			productList.put(product);
+			productList.add(product);
+			bw = new BufferedWriter(new FileWriter(  
+			        file.getAbsolutePath())); 
+			bw.write(dataJson.toString());
+			
+			resp.put("ProductList", productList);
+			bw.close();
+			
+
+		}
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}// 读取原始json文件  
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+     
+
+		return resp;
+	}
+	
+	@DELETE
+	@Path("/deleteProduct")
+    @Consumes(MediaType.APPLICATION_JSON)  
+    @Produces(MediaType.APPLICATION_JSON)  
+	public JSONObject deleteProduct(@QueryParam("product") String product){
+		JSONObject resp = new JSONObject();
+		User user = new User();
+		user.setAge(10);
+		String separator = File.separator;
+		String fileName = "ProductList.txt";
+		String directory = ".." + separator + ".." + separator;
+        File file = new File(directory,fileName);
+        BufferedReader br;
+        BufferedWriter bw; 
+        String str =file.getAbsolutePath();
+		try {
+			br = new BufferedReader(new FileReader(  
+			        file.getAbsolutePath()));   
+			String full_content = "", temp_str = "";  
+			while ((temp_str = br.readLine()) != null){
+				full_content = full_content + temp_str;
+			}
+			br.close();
+			JSONObject dataJson = JSON.parseObject(full_content);
+			JSONArray productList = dataJson.getJSONArray("ProductList");
+			productList.remove(product);
 			bw = new BufferedWriter(new FileWriter(  
 			        file.getAbsolutePath())); 
 			bw.write(dataJson.toString());
